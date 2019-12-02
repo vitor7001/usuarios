91102c1ec10d45e0ae447f6e757f03d21a16fe2e
vitor7001 <vitorgomes7001@gmail.com>
2019-12-02 18:32:25 -0300

    Inserir usuarios

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,15 @@ import index from './pages/index/index'
 
 import Details from './pages/details/details'
 
+import Insert from './pages/insert/insert'
+
 const routes = () => (
     <BrowserRouter>
         <Switch>
 
         <Route exact path="/" component={index} />
         <Route  path="/usuarios/:id" component={Details} />
+        <Route  path="/CriarUsuarios" component={Insert} />
 
         </Switch>
     </BrowserRouter>
