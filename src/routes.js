5caa6be589c4dab4ce51f90430a7911ea0695014
vitor7001 <vitorgomes7001@gmail.com>
2019-12-10 21:06:32 -0300

    Update

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,9 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import index from './pages/index/index'
-
 import Details from './pages/details/details'
-
 import Insert from './pages/insert/insert'
+import Update from './pages/update/update'
 
 const routes = () => (
     <BrowserRouter>
@@ -14,6 +13,8 @@ const routes = () => (
         <Route exact path="/" component={index} />
         <Route  path="/usuarios/:id" component={Details} />
         <Route  path="/CriarUsuarios" component={Insert} />
+        <Route  path="/EditarUsuario/:id" component={Update} />
+
 
         </Switch>
     </BrowserRouter>
